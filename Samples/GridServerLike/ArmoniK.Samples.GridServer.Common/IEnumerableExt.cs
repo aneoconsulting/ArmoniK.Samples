e47bdb72148761e182226ede31cd34f86fda6469
ddubuc <ddubuc@aneo.fr>
2022-05-16 02:46:21 +0200

    Integrate dotnet 4.8 in Samples

---
+++ Samples/GridServerLike/ArmoniK.Samples.GridServer.Common/IEnumerableExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmoniK.Samples.GridServer.Common
+{
+  public static class SelectExtensions
+  {
+    public static IEnumerable<double> ConvertToArray(this IEnumerable<byte> arr)
+    {
+      var bytes = arr as byte[] ?? arr.ToArray();
+
+      var values = new double[bytes.Count() / sizeof(double)];
+
+      var i = 0;
+      for (; i < values.Length; i++)
+        values[i] = BitConverter.ToDouble(bytes.ToArray(),
+          i * 8);
+      return values;
+    }
+  }
+}
