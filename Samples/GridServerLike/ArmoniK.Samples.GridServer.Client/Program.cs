a5619b09b11c5982c498e932d59daf86536d2daa
ddubuc <ddubuc@aneo.fr>
2022-05-17 20:39:42 +0200

    Update Name of packages for GridServer

--- Samples/GridServerLike/ArmoniK.Samples.GridServer.Client/Program.cs
+++ Samples/GridServerLike/ArmoniK.Samples.GridServer.Client/Program.cs
@@ -1,5 +1,5 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2022.
 //   W. Kirschenmann   <wkirschenmann@aneo.fr>
 //   J. Gurhem         <jgurhem@aneo.fr>
@@ -9,13 +9,13 @@
 //   S. Djebbar        <sdjebbar@aneo.fr>
 //   J. Fonseca        <jfonseca@aneo.fr>
 //   D. Brasseur       <dbrasseur@aneo.fr>
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -156,7 +156,7 @@ namespace ArmoniK.Samples.GridServer.Client
                               EngineType.DataSynapse.ToString());
 
       taskOptions.Options.Add(AppsOptions.GridAppNameKey,
-                              "ArmoniK.Samples.GridServer.Client");
+                              "ArmoniK.Samples.GridServer.Services");
 
       taskOptions.Options.Add(AppsOptions.GridAppVersionKey,
                               "1.0.0-700");
