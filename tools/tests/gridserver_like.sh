d76a9cb828cfb8be6ced434d92b00426118cc675
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-06-10 17:37:17 +0200

    Add option in tests to not copy a dll in a shared storage

--- tools/tests/gridserver_like.sh
+++ tools/tests/gridserver_like.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 set -e
 
-export MODE="All"
-export SERVER_NFS_IP=""
-export STORAGE_TYPE="HostPath"
-configuration=Debug
-FRAMEWORK=net6.0
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+DRY_RUN="${DRY_RUN:-0}"
+
+configuration="Debug"
+FRAMEWORK="net6.0"
 OUTPUT_JSON="nofile"
 TO_BUCKET=false
+TO_DIRECTORY=true
 PACKAGE_NAME="ArmoniK.Samples.GridServer.Services-v1.0.0-700.zip"
 RELATIVE_PROJECT="../../Samples/GridServerLike"
 RELATIVE_CLIENT="ArmoniK.Samples.GridServer.Client"
-DEFAULT=FALSE
 args=""
 
 BASEDIR=$(dirname "$0")
 pushd $BASEDIR
 BASEDIR=$(pwd -P)
 popd
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-DRY_RUN="${DRY_RUN:-0}"
 
 pushd $(dirname $0) >/dev/null 2>&1
 BASEDIR=$(pwd -P)
@@ -34,7 +31,7 @@ cd ${TestDir}
 export CPIP=$(kubectl get svc ingress -n armonik -o custom-columns="IP:.spec.clusterIP" --no-headers=true)
 export CPPort=$(kubectl get svc ingress -n armonik -o custom-columns="PORT:.spec.ports[1].port" --no-headers=true)
 export Grpc__Endpoint=http://$CPIP:$CPPort
-export Grpc__SSLValidation="false"
+export Grpc__SSLValidation="disable"
 export Grpc__CaCert=""
 export Grpc__ClientCert=""
 export Grpc__ClientKey=""
@@ -43,9 +40,9 @@ export Grpc__mTLS="false"
 nuget_cache=$(dotnet nuget locals global-packages --list | awk '{ print $2 }')
 
 function createLocalDirectory() {
-    if [[ ${TO_BUCKET} == false ]]; then
+    if [[ ${TO_BUCKET} == false && ${TO_DIRECTORY} == true ]]; then
       echo "Need to create Data folder \"${HOME}/data\" for application"
-      mkdir -p ${HOME}/data
+      mkdir -p "${HOME}/data"
     fi
 }
 
@@ -62,18 +59,18 @@ function SSLConnection()
 function GetGrpcEndPointFromFile()
 {
   OUTPUT_JSON=$1
-  if [ -f ${OUTPUT_JSON} ]; then
+  if [ -f "${OUTPUT_JSON}" ]; then
     #Test if ingress exists
     echo "Input file: ${OUTPUT_JSON}"
-    link=`cat ${OUTPUT_JSON} | jq -r -e '.armonik.ingress.control_plane_url'`
+    link=`cat "${OUTPUT_JSON}" | jq -r -e '.armonik.ingress.control_plane_url'`
     if [ "$?" == "1" ]; then
-      link=`cat ${OUTPUT_JSON} | jq -r -e '.armonik.control_plane_url'`
+      link=`cat "${OUTPUT_JSON}" | jq -r -e '.armonik.control_plane_url'`
       if [ "$?" == "1" ]; then
         echo "Error : cannot read Endpoint from file ${OUTPUT_JSON}"
         exit 1
       fi
     fi
-    export Grpc__Endpoint=$link
+    export Grpc__Endpoint="$link"
   fi
   echo "Running with endPoint ${Grpc__Endpoint} from output.json"
 }
@@ -85,28 +82,30 @@ function GetGrpcEndPoint()
 }
 
 function build() {
-  cd ${TestDir}/
-  echo rm -rf ${nuget_cache}/armonik.*
-  rm -rf $(dotnet nuget locals global-packages --list | awk '{ print $2 }')/armonik.*
+  cd "${TestDir}/"
+  echo "rm -rf ${nuget_cache}/armonik.*"
+  rm -rf "$(dotnet nuget locals global-packages --list | awk '{ print $2 }')/armonik.*"
   find \( -iname obj -o -iname bin \) -exec rm -rf {} +
   dotnet publish --self-contained -c ${configuration} -r linux-x64 -f ${FRAMEWORK} .
 }
 
 function deploy() {
-  cd ${TestDir}
+  cd "${TestDir}"
   if [[ ${TO_BUCKET} == true ]]; then
     export S3_BUCKET=$(aws s3api list-buckets --output json | jq -r '.Buckets[].Name' | grep "s3fs")
     echo "Copy of S3 Bucket ${TO_BUCKET}"
     aws s3 cp "packages/${PACKAGE_NAME}" "s3://${S3_BUCKET}"
-  else
+  elif [[ ${TO_DIRECTORY} == true ]]; then
     cp -v "packages/${PACKAGE_NAME}" "${HOME}/data"
+  else
+    echo "Dll is not copied to shared storage !"
   fi
   kubectl delete -n armonik $(kubectl get pods -n armonik -l service=compute-plane --no-headers=true -o name) || true
 }
 
 function execute() {
   echo "cd ${TestDir}/${RELATIVE_CLIENT}/"
-  cd ${TestDir}/${RELATIVE_CLIENT}/
+  cd "${TestDir}/${RELATIVE_CLIENT}/"
   echo dotnet run -r linux-x64 -f net6.0 -c ${configuration} $@
   dotnet run -r linux-x64 -f net6.0 -c ${configuration} $@
 }
@@ -124,6 +123,7 @@ function usage() {
         -r | --run          : To run only deploy package and test
         -a                  : To run only deploy package and test
         -s3                 : Need S3 copy with aws cp
+        -no-copy-dll        : Don't copy dll anywhere
 EOF
   echo
   exit 0
@@ -148,10 +148,17 @@ while [ $# -ne 0 ]; do
       SSLConnection
       shift
       ;;
+
     -s3)
       TO_BUCKET=true
       shift
       ;;
+
+    -no-copy-dll)
+      TO_DIRECTORY=false
+      shift
+      ;;
+
     -e | --endpoint)
       GetGrpcEndPoint "$2"
       shift
