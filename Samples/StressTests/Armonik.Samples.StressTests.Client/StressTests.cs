a71e5d4c0752d74719582c97dd0f00f2a63f9313
ddubuc <ddubuc@aneo.fr>
2022-12-07 23:52:40 +0100

    Push StressTests

---
+++ Samples/StressTests/Armonik.Samples.StressTests.Client/StressTests.cs
@@ -0,0 +1,217 @@
+// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2021-$CURRENT_YEAR$. All rights reserved.
+//   W. Kirschenmann   <wkirschenmann@aneo.fr>
+//   J. Gurhem         <jgurhem@aneo.fr>
+//   D. Dubuc          <ddubuc@aneo.fr>
+//   L. Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F. Lemaitre       <flemaitre@aneo.fr>
+//   S. Djebbar        <sdjebbar@aneo.fr>
+//   J. Fonseca        <jfonseca@aneo.fr>
+//   D. Brasseur       <dbrasseur@aneo.fr>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Diagnostics;
+
+using ArmoniK.Api.gRPC.V1;
+using ArmoniK.DevelopmentKit.Client.Common;
+using ArmoniK.DevelopmentKit.Client.Common.Exceptions;
+using ArmoniK.DevelopmentKit.Client.Unified.Factory;
+using ArmoniK.DevelopmentKit.Client.Unified.Services.Admin;
+using ArmoniK.DevelopmentKit.Client.Unified.Services.Submitter;
+using ArmoniK.DevelopmentKit.Common;
+using ArmoniK.Samples.Common;
+
+using Google.Protobuf.WellKnownTypes;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Armonik.Samples.StressTests.Client
+{
+  internal class StressTests
+  {
+    public StressTests(IConfiguration configuration,
+                       ILoggerFactory factory,
+                       string         partition)
+    {
+      TaskOptions = new TaskOptions
+                    {
+                      MaxDuration = new Duration
+                                    {
+                                      Seconds = 3600 * 24,
+                                    },
+                      MaxRetries           = 3,
+                      Priority             = 1,
+                      EngineType           = EngineType.Unified.ToString(),
+                      ApplicationVersion   = "1.0.0-700",
+                      ApplicationService   = "ServiceApps",
+                      ApplicationName      = "Armonik.Samples.StressTests.Worker",
+                      ApplicationNamespace = "Armonik.Samples.StressTests.Worker",
+                      PartitionId          = partition,
+                    };
+
+      Props = new Properties(TaskOptions,
+                             configuration.GetSection("Grpc")["EndPoint"])
+              {
+                MaxConcurrentBuffer = 2,
+                MaxTasksPerBuffer   = 50,
+                MaxParallelChannel  = 2,
+                TimeTriggerBuffer   = TimeSpan.FromSeconds(5),
+              };
+
+      Logger = factory.CreateLogger<StressTests>();
+
+      Service = ServiceFactory.CreateService(Props,
+                                             factory);
+      ServiceAdmin = ServiceFactory.GetServiceAdmin(Props,
+                                                    factory);
+      ResultHandle = new ResultForStressTestsHandler(Logger);
+    }
+
+    public ServiceAdmin ServiceAdmin { get; set; }
+
+    private ResultForStressTestsHandler ResultHandle { get; }
+
+    public ILogger<StressTests> Logger { get; set; }
+
+    public Properties Props { get; set; }
+
+    public TaskOptions TaskOptions { get; set; }
+
+    private Service Service { get; }
+
+    internal void LargePayloadSubmit(int  nbTasks          = 100,
+                                     long nbInputBytes     = 64000,
+                                     long nbOutputBytes    = 8,
+                                     int  workloadTimeInMs = 1)
+    {
+      var periodicInfo = ComputeVector(nbTasks,
+                                       nbInputBytes,
+                                       nbOutputBytes,
+                                       workloadTimeInMs);
+
+      Service.Dispose();
+      periodicInfo.Dispose();
+
+      Logger.LogInformation($"Total result is {ResultHandle.Total}");
+
+    }
+
+    /// <summary>
+    ///   The first test developed to validate dependencies subTasking
+    /// </summary>
+    /// <param name="nbTasks">The number of task to submit</param>
+    /// <param name="nbInputBytes">The number of element n x M in the vector</param>
+    /// <param name="nbOutputBytes">The number of bytes to expect as result</param>
+    /// <param name="workloadTimeInMs">The time spent to compute task</param>
+    private IDisposable ComputeVector(int  nbTasks,
+                                      long nbInputBytes,
+                                      long nbOutputBytes    = 8,
+                                      int  workloadTimeInMs = 1)
+    {
+      var       indexTask = 0;
+      var       prevIndex = 0;
+      const int elapsed   = 30;
+
+      var inputArrayOfBytes = Enumerable.Range(0,
+                                               (int)(nbInputBytes / 8))
+                                        .Select(x => Math.Pow(42.0 * 8 / nbInputBytes,
+                                                              1        / 3.0))
+                                        .ToArray();
+
+      Logger.LogInformation($"===  Running from {nbTasks} tasks with payload by task {nbInputBytes / 1024.0} Ko Total : {nbTasks * nbInputBytes / 1024.0} Ko...   ===");
+      var sw = Stopwatch.StartNew();
+      var periodicInfo = Utils.PeriodicInfo(() =>
+                                            {
+                                              Logger.LogInformation($"{indexTask}/{nbTasks} Tasks. " + $"Got {ResultHandle.NbResults} results. " +
+                                                                    $"Check Submission perf : Payload {(indexTask - prevIndex) * nbInputBytes / 1024.0 / elapsed:0.0} Ko/s (inst), " +
+                                                                    $"{(indexTask - prevIndex) / (double)elapsed:0.00} tasks/s (inst), " +
+                                                                    $"{indexTask * 1000.0 / sw.ElapsedMilliseconds:0.00} task/s (avg), " +
+                                                                    $"{indexTask * nbInputBytes / 1024.0 / (sw.ElapsedMilliseconds / 1000.0):0.00} Ko/s (avg)");
+                                              prevIndex = indexTask;
+                                            },
+                                            elapsed);
+
+      var result = Enumerable.Range(0,
+                                    nbTasks)
+                             .Select(idx => Service.SubmitAsync("ComputeWorkLoad",
+                                                                Utils.ParamsHelper(inputArrayOfBytes,
+                                                                                   nbOutputBytes,
+                                                                                   workloadTimeInMs),
+                                                                ResultHandle));
+
+      var taskIds = Task.WhenAll(result).Result.ToHashSet();
+      indexTask = taskIds.Count();
+
+      Logger.LogInformation($"{taskIds.Count}/{nbTasks} tasks executed in : {sw.ElapsedMilliseconds / 1000.0:0.00} secs with Total bytes {nbTasks * nbInputBytes / 1024.0:0.00} Ko");
+
+      return periodicInfo;
+    }
+
+    private class ResultForStressTestsHandler : IServiceInvocationHandler
+    {
+      private readonly ILogger<StressTests> Logger_;
+
+      public ResultForStressTestsHandler(ILogger<StressTests> Logger)
+        => Logger_ = Logger;
+
+      public int NbResults { get; private set; }
+      public double Total { get; private set; }
+
+      /// <summary>
+      ///   The callBack method which has to be implemented to retrieve error or exception
+      /// </summary>
+      /// <param name="e">The exception sent to the client from the control plane</param>
+      /// <param name="taskId">The task identifier which has invoke the error callBack</param>
+      public void HandleError(ServiceInvocationException e,
+                              string                     taskId)
+
+      {
+        if (e.StatusCode == ArmonikStatusCode.TaskCanceled)
+        {
+          Logger_.LogWarning($"Warning from {taskId} : " + e.Message);
+        }
+        else
+        {
+          Logger_.LogError($"Error from {taskId} : " + e.Message);
+          throw new ApplicationException($"Error from {taskId}",
+                                         e);
+        }
+      }
+
+      /// <summary>
+      ///   The callBack method which has to be implemented to retrieve response from the server
+      /// </summary>
+      /// <param name="response">The object receive from the server as result the method called by the client</param>
+      /// <param name="taskId">The task identifier which has invoke the response callBack</param>
+      public void HandleResponse(object response,
+                                 string taskId)
+
+      {
+        switch (response)
+        {
+          case double[] doubles:
+            Total += doubles.Sum();
+            break;
+          case null:
+            Logger_.LogInformation("Task finished but nothing returned in Result");
+            break;
+        }
+
+        NbResults++;
+      }
+    }
+  }
+}
---
+++ Samples/StressTests/Armonik.Samples.StressTests.Worker/ServiceApps.cs
@@ -0,0 +1,116 @@
+// This file is part of the ArmoniK project
+// 
+// Copyright (C) ANEO, 2021-2022.
+//   W. Kirschenmann   <wkirschenmann@aneo.fr>
+//   J. Gurhem         <jgurhem@aneo.fr>
+//   D. Dubuc          <ddubuc@aneo.fr>
+//   L. Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F. Lemaitre       <flemaitre@aneo.fr>
+//   S. Djebbar        <sdjebbar@aneo.fr>
+//   J. Fonseca        <jfonseca@aneo.fr>
+//   D. Brasseur       <dbrasseur@aneo.fr>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+using ArmoniK.DevelopmentKit.Common.Exceptions;
+using ArmoniK.DevelopmentKit.Worker.Unified;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+
+namespace Armonik.Samples.StressTests.Worker
+{
+  public class ServiceApps : BaseService<ServiceApps>
+  {
+    private readonly IConfiguration       configuration_;
+    private readonly ILogger<ServiceApps> logger_;
+
+    public ServiceApps()
+    {
+      var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                              .AddJsonFile("appsettings.json",
+                                                           true,
+                                                           true)
+                                              .AddEnvironmentVariables();
+
+
+      configuration_ = builder.Build();
+
+      Log.Logger = new LoggerConfiguration().MinimumLevel.Override("Microsoft",
+                                                                   LogEventLevel.Information)
+                                            .ReadFrom.Configuration(configuration_)
+                                            .Enrich.FromLogContext()
+                                            .WriteTo.Console()
+                                            .CreateBootstrapLogger();
+
+      var logProvider = new SerilogLoggerProvider(Log.Logger);
+      var factory = new LoggerFactory(new[]
+                                      {
+                                        logProvider,
+                                      });
+
+      logger_ = factory.CreateLogger<ServiceApps>();
+    }
+
+
+    public double[] ComputeWorkLoad(double[] input,
+                                    long      nbOutputBytes,
+                                    int      workLoadTimeInMs)
+    {
+      if (input is not
+          {
+            Length: > 0,
+          } || nbOutputBytes <= 0)
+      {
+        logger_.LogInformation("Cannot execute function with nb element <= 0");
+        throw new WorkerApiException("Cannot execute function with nb bytes <= 0");
+      }
+
+      if (workLoadTimeInMs <= 0)
+      {
+        workLoadTimeInMs = 1;
+      }
+
+      var output = Enumerable.Range(0,
+                                    (int)(nbOutputBytes / 8))
+                             .Select(x => (double)x)
+                             .ToArray();
+
+      var result = input.Select(x => Math.Pow(x,
+                                              3.0))
+                        .Sum();
+      // Record start time
+      var start = Stopwatch.StartNew();
+
+      while (start.ElapsedMilliseconds < workLoadTimeInMs)
+      {
+        for (var rIdx = 0; rIdx < output.Length; rIdx++)
+        {
+          output[rIdx] = result / output.Length;
+        }
+      }
+
+
+      return output;
+    }
+  }
+}
