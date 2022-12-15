fc39cdd94d65593229e4afbf36d327003c9b455b
ddubuc <ddubuc@aneo.fr>
2022-12-15 15:56:39 +0100

    fix build

--- Samples/HtcMockSymphony/ArmoniK.Samples.HtcMockSymphonyPackage/ServiceContainer.cs
+++ Samples/HtcMockSymphony/ArmoniK.Samples.HtcMockSymphonyPackage/ServiceContainer.cs
@@ -28,8 +28,7 @@ using System.Linq;
 using System.Text;
 
 using ArmoniK.DevelopmentKit.Common.Exceptions;
-using ArmoniK.DevelopmentKit.SymphonyApi;
-using ArmoniK.DevelopmentKit.SymphonyApi.api;
+using ArmoniK.DevelopmentKit.Worker.Symphony;
 
 using Htc.Mock.Core;
 
