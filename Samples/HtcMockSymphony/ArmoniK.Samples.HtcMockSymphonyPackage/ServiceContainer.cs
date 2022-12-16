cba468eedcc942c7cac65d9f36c1f922d46b7852
ddubuc <ddubuc@aneo.fr>
2022-12-16 11:33:00 +0100

    Fix format

--- Samples/HtcMockSymphony/ArmoniK.Samples.HtcMockSymphonyPackage/ServiceContainer.cs
+++ Samples/HtcMockSymphony/ArmoniK.Samples.HtcMockSymphonyPackage/ServiceContainer.cs
@@ -27,7 +27,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using ArmoniK.DevelopmentKit.Common.Exceptions;
 using ArmoniK.DevelopmentKit.Worker.Symphony;
 
 using Htc.Mock.Core;
