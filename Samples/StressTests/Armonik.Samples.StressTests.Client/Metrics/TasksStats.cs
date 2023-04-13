3241583b479c0bb93591b344ff76928c96216518
Yanis KHAROUNI <ykharouni@aneo.fr>
2023-04-13 17:46:52 +0200

    format code

--- Samples/StressTests/Armonik.Samples.StressTests.Client/Metrics/TasksStats.cs
+++ Samples/StressTests/Armonik.Samples.StressTests.Client/Metrics/TasksStats.cs
@@ -38,7 +38,7 @@ namespace Armonik.Samples.StressTests.Client.Metrics
   {
     public enum KpiKeys
     {
-      TEST                       = 0, 
+      TEST                       = 0,
       COMPLETED_TASKS            = 1,
       TIME_SUBMITTED_TASKS       = 2,
       TIME_THROUGHPUT_SUBMISSION = 3,
