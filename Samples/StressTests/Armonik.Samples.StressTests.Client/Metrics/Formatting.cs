79cbf099a36c1cb3591d50517bca639845fa7a56
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-06-19 17:37:10 +0200

    chore: update ReSharper version to 2025.1.2 in code formatting workflow

--- Samples/StressTests/Armonik.Samples.StressTests.Client/Metrics/Formatting.cs
+++ Samples/StressTests/Armonik.Samples.StressTests.Client/Metrics/Formatting.cs
@@ -1,6 +1,6 @@
 // This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2021-$CURRENT_YEAR$. All rights reserved.
+// 
+// Copyright (C) ANEO, 2021-2025. All rights reserved.
 //   W. Kirschenmann   <wkirschenmann@aneo.fr>
 //   J. Gurhem         <jgurhem@aneo.fr>
 //   D. Dubuc          <ddubuc@aneo.fr>
@@ -13,12 +13,12 @@
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
